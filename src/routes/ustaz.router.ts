e060cc30fcd3f7b0c85b9552c5a6109d7306323d
ibrahmad <ibrahmad18@gmail.com>
2023-08-26 15:44:58 +0100

    config ustaz route to the main router

--- src/routes/ustaz.router.ts
+++ src/routes/ustaz.router.ts
@@ -2,5 +2,23 @@ const express   =  require('express')
 const router    =  express.Router()
 
 import {
-
+    addUstaz,
+    getAllUstazs,
+    getUstazById,
+    updateUstazById,
+    deleteUstazById
 } from '../controllers/ustaz.controller'
+
+router.get('/all', getAllUstazs)
+router.get('/', getUstazById)
+
+// add ustaz
+router.post('/', addUstaz)
+
+// update ustaz by id
+router.put('/:id', updateUstazById)
+
+// delete ustaz
+router.delete('/:id', deleteUstazById)
+
+export default router;
