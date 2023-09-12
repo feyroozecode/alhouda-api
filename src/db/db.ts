1d6016f608c346f03952948c5a50fc649a575dc4
ibrahmad <ibrahmad18@gmail.com>
2023-09-12 09:33:04 +0100

    adding of .env and configure successfully

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,14 @@
 import { DB_DATA } from "../static_data/db_datas"
 
 const mongoose  = require('mongoose')
-const localDb   = `mongodb://localhost:27017/${DB_DATA.DATATABASE_NAME}`
+const LOCAL_DB_NAME = `mongodb://localhost:27017/${DB_DATA.DATATABASE_NAME}`
+const DATABASE_URL  = process.env.DATABASE_NAME || LOCAL_DB_NAME
 
 /// connecting to database  
 
 export const connectToDb = async () => {
     try {
-        await mongoose.connect(localDb, {
+        await mongoose.connect(DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         } )
