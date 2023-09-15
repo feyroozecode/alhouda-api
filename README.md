6fd80a284d28858ea2dbab5051a2decbd24edc84
ibrahmad <ibrahmad18@gmail.com>
2023-09-15 17:59:38 +0100

    adding dotenv to db.ts and update readme

--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 ## Alhouda API v.1 
   [This project is using NodeJs, Typescript and somes package like : express, mongoose etc...]
 
-- Alhouda Project is a Platform for Islam with local Ulema NigerNigeria scholars Courses, Lectures, Quran , Hadith , articles etc.. 
-
+- Alhouda Project is a Platform for Islam with local Ulema NigerNigeria scholars Courses, Lectures, Quran , Hadith , articles etc..
--- src/db/db.ts
+++ src/db/db.ts
@@ -5,12 +5,12 @@ const LOCAL_DB_NAME = `mongodb://localhost:27017/${DB_DATA.DATATABASE_NAME}`
 const DATABASE_URL  = process.env.DATABASE_NAME_URL || LOCAL_DB_NAME
 
 /// connecting to database  
-
 export const connectToDb = async () => {
     try {
-        await mongoose.connect(DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        await mongoose.connect(
+            DATABASE_URL, {
+                useNewUrlParser: true,
+                    useUnifiedTopology: true
         } )
         console.log('connected to db')
     }
