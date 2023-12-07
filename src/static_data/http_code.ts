6e8141e7337bebc97bbd5cc7f282fd60aa8ace0d
Ibrahim Ahmad (feyroozecode) <43380715+feyroozecode@users.noreply.github.com>
2023-12-07 23:00:04 +0100

    Update http_code.ts

--- src/static_data/http_code.ts
+++ src/static_data/http_code.ts
@@ -3,6 +3,7 @@
 export const HTTP_CODE = {
     OK                    :             200 , 
     CREATED               :             201 ,
+    NO_CONTENT            :             204 , 
     BAD_REQUEST           :             400 ,
     UNAUTHORIZED          :             401 ,
     NOT_FOUND             :             404 ,
