463923aa673cd96a258e0b6192b32b9dc6ee2266
Ibrahim Ahmad (feyroozecode) <43380715+feyroozecode@users.noreply.github.com>
2023-12-09 14:15:13 +0100

    Create action_handler.factory.ts
    init a facotry method , base of all the prijects

---
+++ src/services/action_handler.factory.ts
@@ -0,0 +1,23 @@
+/// implement base method for handling all user methods for CRUD
+
+import {express, Request, Response} from 'express';
+
+// create one 
+export const createOne = (Model: any) => {
+  
+}
+
+// update one
+export const updateOne = (Model: any) => {
+  
+}
+
+// delete one
+export const deleteOne = (Model: any) => {
+  
+}
+
+// get all 
+export const getAll = (Model: any) => {
+  
+}
