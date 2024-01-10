41aae5756f8127df66bdfc5095192c31e13a6190
ibrahmad <ibrahmad18@gmail.com>
2024-01-10 23:36:41 +0100

    update of db, and some things

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,17 +4,20 @@ const mongoose  = require('mongoose')
 const LOCAL_DB_NAME = `mongodb://localhost:27017/${DB_DATA.DATATABASE_NAME}`
 const DATABASE_URL  = process.env.DATABASE_NAME_URL || LOCAL_DB_NAME
 
-/// connecting to database  
+/// connecting to database
 export const connectToDb = async () => {
+
     try {
         await mongoose.connect(
             DATABASE_URL, {
                 //useNewUrlParser: true,
-                // useUnifiedTopology: true
+                //useUnifiedTopology: true
         } )
         console.log('connected to db')
     }
+
     catch (error) {
         console.log(error)
     }
+
 }
